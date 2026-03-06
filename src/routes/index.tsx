e4e93404805c5b89633fb4e9cc48e5474023dd4e
Trieu Van Quyen <quyenanc@gmail.com>
2026-03-07 00:31:57 +0700

    buid ui roomdetail

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 import AuthPage from '../pages/auth/pages/AuthPage'
 import HomePage from '../components/Layouts/HomePage'
+import RoomDetailPage from '../pages/products/pages/roomdetail/pages/RoomDetail'
 const Routers = () => {
     return (
         <Routes>
             <Route path='/auth' element={<AuthPage />} />
             <Route path='/' element={<HomePage />} />
-
+            <Route path='/room/detail/:id' element={<RoomDetailPage />} />
         </Routes>
     )
 }
